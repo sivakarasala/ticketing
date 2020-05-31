292dbfae8868bd7ae1d760d82acb202a9cba32c4
Siva Krishna <shivataraya@gmail.com>
2020-05-30 20:43:57 -0400

    add jwt/cookie auth to signup, configure secret key access to auth-depl

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+
 import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { BadRequestError } from "../errors/bad-request-error";
@@ -34,6 +36,18 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store it on session object
+    req.session!.jwt! = userJwt;
 
     return res.status(201).send(user);
   }
