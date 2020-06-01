dc3705442a7542e054b750ff2d46bff57ec018db
Siva Krishna <shivataraya@gmail.com>
2020-06-01 02:32:54 -0400

    configure homepage to get currentuser status

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,1 +1,18 @@
-export default () => <h1>Aum Namah Sivaya</h1>;
+import buildClient from "../api/build-client";
+
+const HomePage = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in </h1>
+  ) : (
+    <h1> You are not signed in</h1>
+  );
+};
+
+HomePage.getInitialProps = async context => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+
+  return data;
+};
+
+export default HomePage;
