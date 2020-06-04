01031b026f5cfc04b0ec861a5ff5b53c3f5263bc
Siva Krishna <shivataraya@gmail.com>
2020-06-04 15:08:04 -0400

    add createTicket route with tests

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,12 +1,11 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
-import { app } from "../app";
-import request from "supertest";
+import jwt from "jsonwebtoken";
 
 declare global {
   namespace NodeJS {
     interface Global {
-      signin(): Promise<string[]>;
+      signin(): string[];
     }
   }
 }
@@ -36,19 +35,25 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
-  const email = "siva@siva.com";
-  const password = "aumnamahsivaya";
+global.signin = () => {
+  // Build a JWT payload. { id, email }
+  const payload = {
+    id: "slokyayacha",
+    email: "siva@siva.com"
+  };
 
-  const response = await request(app)
-    .post("/api/users/signup")
-    .send({
-      email,
-      password
-    })
-    .expect(201);
+  // Create the JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-  const cookie = response.get("Set-Cookie");
+  // Build session Object. { jwt: MY_JWT }
+  const session = { jwt: token };
 
-  return cookie;
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  // return a string that the cookie with the encoded data
+  return [`express:sess=${base64}`];
 };
